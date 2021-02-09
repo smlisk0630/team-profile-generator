0bea32d4a3334f4eb79cb9ddbd0f22911330db6d
slisk <63676970+smlisk0630@users.noreply.github.com>
2021-02-09 08:39:24 -0500

    Updated all JS files and created generator file

--- index.js
+++ index.js
@@ -1,58 +1,7 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
-const jest = require('jest');
+const Generator = require('./lib/generator');
 
-// Inputs for the command line
-const manager = inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: "What is the team manager's name?",
-            name: 'team'
-        },
-        {
-            type: 'input',
-            message: "What is the team manager's id?",
-            name: 'id'
-        },
-        {
-            type: 'input',
-            message: "What is the team manager's email address?", 
-            name: 'email'
-        },
-        {
-            type: 'input',
-            message: "What is the team manager's office number?",
-            name: 'office'
-        },
-        {
-            type: 'input',
-            message: "What type of team manager is this?",
-            name: 'this'
-        },
-        {
-            type: 'checkbox',
-            message: "Would you like to select an intern or an engineer?",
-            name: 'next-menu',
-            choices: ["Intern", "Engineer"]
-        },
-    ])
-    .then(response => {
-        const content = `
-        ${response.team}
-        ${response.id}
-        ${response.email}
-        ${response.office}
-        ${response.this}
-        ${response.next-menu}
-        `;
+// Initialize new profile Generator object
+const generator = new Generator();
 
-         // creates HTML file
-         fs.writeFile('dist/index.html', content, err => {
-            if (err) console.log(err);
-            else console.log('success!');
-        });
-    });
-    for(const prompt of manager){
-
-    }
+// Start profile generation
+generator.init();
