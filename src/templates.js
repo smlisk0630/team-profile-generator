7aeac14ba02f5060e56d70abf7fda8262f591bd8
slisk <63676970+smlisk0630@users.noreply.github.com>
2021-02-13 14:23:11 -0500

    Created template.js file

---
+++ src/templates.js
@@ -0,0 +1,15 @@
+// Creates row in which profile cards will live
+rowTemplate (contents => {
+    `<section class="row justify-content-center">${contents}</section>`
+});
+
+// Creates profile card
+function cardTemplate(user) {
+    return `
+
+    `
+}
+// Creates HTML page structure
+function createHTML() {
+
+}
